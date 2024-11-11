7d1a9ede1967c8dbb08cedf3ba6018a705d78e0c
Tang-Annan <1924615475@qq.com>
2024-11-11 14:51:41 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,57 @@
+import math
+from enum import Enum
+import numpy as np
+
+import sys 
+sys.path.append(r'E:\BaiduSyncdisk\path planning')
+from MAP.map1 import ox,oy,gx,gy,sx,sy
+
+class RobotType(Enum):
+    circle = 0
+    rectangle = 1
+
+
+class Config:
+    """
+    simulation parameter class
+    """
+
+    def __init__(self):
+        # robot parameter
+        self.max_speed = 1.0  # [m/s]
+        self.min_speed = -0.5  # [m/s]
+        self.max_yaw_rate = 40.0 * math.pi / 180.0  # [rad/s]
+        self.max_accel = 0.2  # [m/ss]
+        self.max_delta_yaw_rate = 40.0 * math.pi / 180.0  # [rad/ss]
+        self.v_resolution = 0.01  # [m/s]
+        self.yaw_rate_resolution = 0.1 * math.pi / 180.0  # [rad/s]
+        self.dt = 0.1  # [s] Time tick for motion prediction
+        self.predict_time = 1.0  # [s]
+        self.to_goal_cost_gain = 0.15
+        self.speed_cost_gain = 1.0
+        self.obstacle_cost_gain = 1.0
+        self.robot_stuck_flag_cons = 0.001  # constant to prevent robot stucked
+        self.x = np.array([sx, sy, math.pi / 8.0, 0.0, 0.0])
+        self.robot_type = RobotType.circle
+        
+        # if robot_type == RobotType.circle
+        # Also used to check if goal is reached in both types
+        self.robot_radius = 1.0  # [m] for collision check
+
+        # if robot_type == RobotType.rectangle
+        self.robot_width = 0.5  # [m] for collision check
+        self.robot_length = 1.2  # [m] for collision check
+        
+        # 地图数据
+        self.ob = np.array(list(zip(ox, oy)))
+        self.grid_size = 1.0
+
+    @property
+    def robot_type(self):
+        return self._robot_type
+
+    @robot_type.setter
+    def robot_type(self, value):
+        if not isinstance(value, RobotType):
+            raise TypeError("robot_type must be an instance of RobotType")
+        self._robot_type = value
